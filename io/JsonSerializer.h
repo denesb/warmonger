286a4d693f2790ea6070629b3b5dbce92ef85344
Denes Botond <dns.botond@gmail.com>
2016-11-21 20:59:01 +0000

    Document all io classes and files

--- io/JsonSerializer.h
+++ io/JsonSerializer.h
@@ -1,3 +1,7 @@
+/** \file
+ * JSON serializer.
+ */
+
 #ifndef IO_JSON_SERIALIZER_H
 #define IO_JSON_SERIALIZER_H
 
@@ -8,23 +12,141 @@
 namespace warmonger {
 namespace io {
 
+/**
+ * JSON serializer.
+ *
+ * Serialize any core game-object to JSON.
+ */
 class JsonSerializer : public Serializer
 {
 public:
+    /**
+     * Construct a json-serializer object.
+     *
+     * The compactness of the produced data can be changed by the format param.
+     * Use QJsonDocument::Indented to produce nice human-readable output or
+     * QJsonDocument::Compact to produce a compact output. The default is
+     * QJsonDocument::Indented.
+     *
+     * \param format the formatting method to use
+     */
     JsonSerializer(QJsonDocument::JsonFormat format = QJsonDocument::Indented);
 
+    /**
+     * Serialize an army.
+     *
+     * \param obj the army
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeArmy(const core::Army* obj) override;
+
+    /**
+     * Serialize an army-type.
+     *
+     * \param obj the army-type
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeArmyType(const core::ArmyType* obj) override;
+
+    /**
+     * Serialize an banner.
+     *
+     * \param obj the banner
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeBanner(const core::Banner* obj) override;
+
+    /**
+     * Serialize an campaign-map.
+     *
+     * \param obj the campaign-map
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeCampaignMap(const core::CampaignMap* obj) override;
+
+    /**
+     * Serialize an civilization.
+     *
+     * \param obj the civilization
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeCivilization(const core::Civilization* obj) override;
+
+    /**
+     * Serialize an faction.
+     *
+     * \param obj the faction
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeFaction(const core::Faction* obj) override;
+
+    /**
+     * Serialize an map-node.
+     *
+     * \param obj the map-node
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeMapNode(const core::MapNode* obj) override;
+
+    /**
+     * Serialize an settlement.
+     *
+     * \param obj the settlement
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeSettlement(const core::Settlement* obj) override;
+
+    /**
+     * Serialize an settlement-type.
+     *
+     * \param obj the settlement-type
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeSettlementType(const core::SettlementType* obj) override;
+
+    /**
+     * Serialize an terrain-type.
+     *
+     * \param obj the terrain-type
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeTerrainType(const core::TerrainType* obj) override;
+
+    /**
+     * Serialize an unit.
+     *
+     * \param obj the unit
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeUnit(const core::Unit* obj) override;
+
+    /**
+     * Serialize an unit-type.
+     *
+     * \param obj the unit-type
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeUnitType(const core::UnitType* obj) override;
+
+    /**
+     * Serialize an world.
+     *
+     * \param obj the world
+     *
+     * \return the raw serialized data
+     */
     QByteArray serializeWorld(const core::World* obj) override;
 
 private:
--- io/Serializer.h
+++ io/Serializer.h
@@ -1,3 +1,7 @@
+/** \file
+ * Serializer interface.
+ */
+
 #ifndef IO_SERIALIZER_H
 #define IO_SERIALIZER_H
 
@@ -23,21 +27,129 @@ class World;
 
 namespace io {
 
+/**
+ * Serializer interface.
+ *
+ * Serialize any core game-object.
+ */
 class Serializer
 {
 public:
+    /**
+     * Serialize an army.
+     *
+     * \param obj the army
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeArmy(const core::Army* obj) = 0;
+
+    /**
+     * Serialize an army-type.
+     *
+     * \param obj the army-type
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeArmyType(const core::ArmyType* obj) = 0;
+
+    /**
+     * Serialize an banner.
+     *
+     * \param obj the banner
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeBanner(const core::Banner* obj) = 0;
+
+    /**
+     * Serialize an campaign-map.
+     *
+     * \param obj the campaign-map
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeCampaignMap(const core::CampaignMap* obj) = 0;
+
+    /**
+     * Serialize an civilization.
+     *
+     * \param obj the civilization
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeCivilization(const core::Civilization* obj) = 0;
+
+    /**
+     * Serialize an faction.
+     *
+     * \param obj the faction
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeFaction(const core::Faction* obj) = 0;
+
+    /**
+     * Serialize an map-node.
+     *
+     * \param obj the map-node
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeMapNode(const core::MapNode* obj) = 0;
+
+    /**
+     * Serialize an settlement.
+     *
+     * \param obj the settlement
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeSettlement(const core::Settlement* obj) = 0;
+
+    /**
+     * Serialize an settlement-type.
+     *
+     * \param obj the settlement-type
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeSettlementType(const core::SettlementType* obj) = 0;
+
+    /**
+     * Serialize an terrain-type.
+     *
+     * \param obj the terrain-type
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeTerrainType(const core::TerrainType* obj) = 0;
+
+    /**
+     * Serialize an unit.
+     *
+     * \param obj the unit
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeUnit(const core::Unit* obj) = 0;
+
+    /**
+     * Serialize an unit-type.
+     *
+     * \param obj the unit-type
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeUnitType(const core::UnitType* obj) = 0;
+
+    /**
+     * Serialize an world.
+     *
+     * \param obj the world
+     *
+     * \return the raw serialized data
+     */
     virtual QByteArray serializeWorld(const core::World* obj) = 0;
 };
 

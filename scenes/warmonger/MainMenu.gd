8585de49f54aed85ad5cb9d7598e9d7645abb1ff
Botond Dénes <bdenes@scylladb.com>
2019-12-15 20:25:38 +0200

    MainMenu: hook up ExitButton

--- scenes/warmonger/MainMenu.gd
+++ scenes/warmonger/MainMenu.gd
@@ -3,4 +3,9 @@ extends Control
 onready var ctx = preload("res://interface/wcontext.gdns").new()
 
 func _ready():
-    $VersionLabel.text = ctx.version
+	$VersionLabel.text = ctx.version
+	get_node("MainMenuPanel/VBoxContainer/ExitButton").connect("pressed", self, "_on_ExitButton_pressed")
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
